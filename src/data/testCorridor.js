faee66f853701f6e44ab9397133d384848f09f4f
Chris58NK <chrisbarltrop58@icloud.com>
2026-02-18 15:12:03 +0000

    Create testCorridor.js

---
+++ src/data/testCorridor.js
@@ -0,0 +1,9 @@
+export const testCorridor = {
+  length: 3200,
+  features: [
+    { type: "lock", name: "Lock A", chainage: 800 },
+    { type: "lock", name: "Lock B", chainage: 1600 },
+    { type: "winding", name: "Winding Hole", chainage: 2400 },
+    { type: "lock", name: "Lock C", chainage: 3200 }
+  ]
+};
