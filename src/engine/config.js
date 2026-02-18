d6d711bcb922513dc7b62bdf7c91f39c0cc4feeb
Chris58NK <chrisbarltrop58@icloud.com>
2026-02-18 14:50:29 +0000

    Create config.js

---
+++ src/engine/config.js
@@ -0,0 +1,8 @@
+export const DEFAULT_CONFIG = {
+  minChainage: 0,
+  maxChainage: 3200,          // metres
+  metresPerMile: 1609.344,
+  defaultSpeedMph: 3,
+  minutesPerLock: 8,
+  inclusiveEnd: true
+};
