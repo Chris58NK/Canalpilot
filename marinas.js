149f841f8912fcda216fcf87cedc49758e0d56e1
Chris58NK <chrisbarltrop58@icloud.com>
2026-03-29 19:43:10 +0100

    Create marinas.js
    New Marinas file sample data, Just Debdale, Crick and NK

---
+++ marinas.js
@@ -0,0 +1,38 @@
+const marinasData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "North Kilworth Marina",
+        "waterway_name": "Leicester Line"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-1.0467, 52.4522]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Debdale Wharf Marina",
+        "waterway_name": "Leicester Line"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-0.9395, 52.5655]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Crick Marina",
+        "waterway_name": "Leicester Line"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-1.1325, 52.3515]
+      }
+    }
+  ]
+};
